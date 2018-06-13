16e024fb02df0b09a30ddcb30a5f074064001f32
Maksim Kunaev <maksimtruesim@gmail.com>
2018-06-14 02:40:55 +0300

    fix again

--- src/index.js
+++ src/index.js
@@ -2,6 +2,4 @@ import React, { Component } from 'react';
 import ReactDOM from 'react-dom';
 import App from './components/App';
 
-// const App = () =><div>shop24</div>
-
 ReactDOM.render(<App />,  document.getElementById('root'));
