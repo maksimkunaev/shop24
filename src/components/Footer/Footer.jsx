728ec3f100c2db20c730d80facb6f36474b5ef17
Maksim Kunaev <maksimtruesim@gmail.com>
2018-06-15 02:46:30 +0300

    styled

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -1,14 +1,80 @@
 import React, { Component } from 'react';
 import './Footer.css'
 
+const links = [
+    {title: 'vk', image: 'vk.png'},
+    {title: 'vk', image: 'fb.png'},
+    {title: 'vk', image: 'ok.png'},
+    {title: 'vk', image: 'r.png'},
+    {title: 'vk', image: 'heart.png'},
+    {title: 'vk', image: 'instagram.png'},
+    {title: 'vk', image: 'youtube.png'},
+]
+
+const info = [
+    {title: 'Каталог товаров', list: ['Одежда и аксессуары', 'Обувь', 'Украшения', 'Красота и здоровье', 'Товары для дома, дачи и отдыха', 'Товары для кухни']},
+    {title: 'Заказ', list: ['Оплата и доставка', 'Возврат', 'Помощь', 'Благотворительный Фонд Константина Хабенского', 'Гарантия на дополнительное обсуживание товара','Пользовательское соглашение']},
+    {title: 'Shop24', list: ['Смотреть прямой эфир', 'Расписание передач', 'Акции', 'Личный кабинет', 'Поиск и карта сайта', ' Карта брендов', 'Обратная связь']},
+    {title: 'Информация', list: ['О канале', 'Сотрудничество', 'Покупайте с нами!', 'Контакты']}
+]
+
 class Footer extends Component {
 
+    renderSocialLinks() {
+        return <div className='footer__social'>
+            <p>SHOP24 в социальных сетях:</p>
+            {links.map((item, i) => <a className='footer__social-links' href="#" key={i}>
+                <img src={`./image/${item.image}`} alt={item.title} /></a>)}
+       </div>
+    }
+
+    renderInfolinks() {
+         return <div className='footer_info-links'>
+            {info.map((item,i) => <div className='footer_info-block' key={i}>
+                <div className='footer_info-block-title'>
+                    <a href='#' className='footer__links'>
+                        {item.title}</a>
+                    </div>
+                        {item.list.map((item, i) => <div className='footer_info-list'>
+                            <a href='#' key={i} className='footer__links'>{item}</a>
+                    </div>)}
+                </div>)}
+         </div>
+    }
+
+
     render() {
+        const links = this.renderSocialLinks();
+        const info = this.renderInfolinks();
+
         return (
             <div className='footer'>
-               <div className='footer__social'>Social</div>
-               <div className='footer__links'>Links</div>
-               <div className='footer__copyright'>Copyright</div>
+                {links}
+
+               <div className='footer__about_cover'>
+                   <div className='footer__about'>
+                        <img className='footer__logo' width='100' height='100' src={`./image/logo.png`} alt='logo.png' />
+                        <div className='footer__about-phones'>
+                            <div className='footer__about-phone'>
+                                8 (800) 500-75-55<span>*</span></div>
+                            <div className='footer__about-phone-rf'>
+                                <span>*</span>Бесплатный  звонок по всей России</div>
+                            <div className='footer__about-phone'>8 (495) 733-96-03</div>
+                        </div>
+
+                        {info}
+                    </div>
+               </div>
+
+
+               <div className='footer__copyright'>
+                    <div className='footer__copyright-text'>
+                        Copyright © Товар ООО «Стиль и Мода», 2014-2016. Все права защищены.<br/>
+                        При использовании материалов сайта ссылка нa www.shop24.com обязательна.<br/>
+                        115193, Москва, ул Кожуховская 5-ая, д.9, помещение VII<br/>
+                        Телефон 8 800 500-75-55
+                    </div>
+               </div>
             </div>
         )
     }
